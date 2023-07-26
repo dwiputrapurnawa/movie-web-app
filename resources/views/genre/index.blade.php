c802193da6bfdc998b9388ed08281ba30578c63b
dwiputrapurnawa <putrapurnawa@gmail.com>
2023-07-26 22:30:08 +0800

    cleaning UI

--- resources/views/genre/index.blade.php
+++ resources/views/genre/index.blade.php
@@ -5,46 +5,55 @@
 @endsection
 
 @section('content')
+
+@if ($movies->count() === 0)
+<div class="bg-white me-auto text-align p-3 d-flex justify-content-center">
+  <img style="height: 20%; width: 20%;" src="/images/notfound.png" alt="data-not-found">
+</div>
+@else
 <div class="bg-white rounded-bottom">
 
-    <div class="bg-dark p-2 rounded-top">
-        <h6 class="text-white">All Movie Orded By {{ $genre->name }}</h6>
-    </div>
+  <div class="bg-dark p-2 rounded-top">
+      <h6 class="text-white">All Movie Orded By {{ $genre->name }}</h6>
+  </div>
 
 
-    <div class="row p-3">
+  <div class="row p-3">
 
-        @foreach ($movies as $movie)
-        <div class="col-lg-4 me-auto">
-            <a class="text-decoration-none" href="/movie/{{ $movie->slug }}">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row g-0">
-                      <div class="col-md-4">
-                        <img src="/{{ $movie->img ?? "images/no-image.jpg" }}" class="img-fluid rounded-start" alt="movie-img">
-                      </div>
-                      <div class="col-md-8">
-                        <div class="card-body">
-                          <h5 class="card-title">{{ $movie->title }}</h5>
-                          @for ($i = 0; $i < $movie->rating; $i++)
-                            <i class="bi bi-star-fill text-warning"></i>
-                          @endfor
+      @foreach ($movies as $movie)
+      <div class="col-lg-4 me-auto">
+          <a class="text-decoration-none" href="/movie/{{ $movie->slug }}">
+              <div class="card mb-3" style="max-width: 540px;">
+                  <div class="row g-0">
+                    <div class="col-md-4">
+                      <img src="/{{ $movie->img ?? "images/no-image.jpg" }}" class="img-fluid rounded-start" alt="movie-img">
+                    </div>
+                    <div class="col-md-8">
+                      <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        @for ($i = 0; $i < $movie->rating; $i++)
+                          <i class="bi bi-star-fill text-warning"></i>
+                        @endfor
 
-                          @for ($i = 0; $i < (5-$movie->rating); $i++)
-                            <i class="bi bi-star text-warning"></i>
-                          @endfor
-                          <p class="card-text"><small class="text-body-secondary">{{ $movie->created_at->diffForHumans() }}</small></p>
-                        </div>
+                        @for ($i = 0; $i < (5-$movie->rating); $i++)
+                          <i class="bi bi-star text-warning"></i>
+                        @endfor
+                        <p class="card-text"><small class="text-body-secondary">{{ $movie->created_at->diffForHumans() }}</small></p>
                       </div>
                     </div>
                   </div>
-            </a>
-        </div>
-        @endforeach
-        
+                </div>
+          </a>
+      </div>
+      @endforeach
+      
 
-        {{ $movies->links() }}
-        
-    </div>
+      {{ $movies->links() }}
+      
+  </div>
 
-</div>
+</div> 
+@endif
+
+
 @endsection
